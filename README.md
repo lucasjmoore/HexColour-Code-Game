5369858774222c9afebe20810076e2d2204a6de7
lucasjmoore <lucasmoore88@gmail.com>
2013-12-03 09:03:34 -0500

    first commit, new random colour on submit, handles invalid, 3, 6 digit hexadecimal colour codes

---
+++ README.md
@@ -0,0 +1,4 @@
+HexColour-Code-Game
+===================
+
+A Hexadecimal color code game. Where the user has to input the Hexadecimal code they think the background colour is and will get scored on how accurate they are.
---
+++ js/script.js
@@ -0,0 +1,95 @@
+  function setRandomColour(){
+
+    var colourDecimalR,colourDecimalG,colourDecimalB;
+
+    colourDecimalR = Math.floor(Math.random()*256);
+    colourDecimalG = Math.floor(Math.random()*256);
+    colourDecimalB = Math.floor(Math.random()*256);
+
+    setBackgroundColour(colourDecimalR,colourDecimalG,colourDecimalB);
+
+  }
+
+  function setBackgroundColour(colourDecimalR, colourDecimalG, colourDecimalB){
+    console.log( 'random colour value: rgb(' + [colourDecimalR,colourDecimalG,colourDecimalB].join(',') + ')');
+    document.body.style.backgroundColor = 'rgb(' + [colourDecimalR,colourDecimalG,colourDecimalB].join(',') + ')';
+  }
+
+
+  function playerColourValueSubmit(){
+
+      var colourGuessValue =document.querySelector("#colourGuessInput").value;
+      console.log("colourGuessValue: " + colourGuessValue);
+
+      checkValidInput(colourGuessValue);
+
+      //hexColourToDecimalSplit(colourGuessValue);
+  }
+
+function checkValidInput(colourGuessValue){
+  if(colourGuessValue.length == 3){
+    //  If colourGuessValue only has 3 characters.  Example: #FFF
+
+    hexColourToDecimalSplit(colourGuessValue);
+
+    //set next random colour
+    setRandomColour();
+  }
+  else if (colourGuessValue.length == 6){
+    //  Else colourGuessValue 6 characters.  Example: #FFFFFF
+    
+    hexColourToDecimalSplit(colourGuessValue);
+
+    //set next random colour
+    setRandomColour();
+  }
+  else{
+    //  Else colourGuessValue has an invalid amount of characters
+    alert("Please enter a valid hexadecimal number.")
+  }
+  
+}
+
+
+function hexColourToDecimalSplit(colourGuessValue){
+  /*
+  find the length of the colour.   Either 3 || 6
+  if length is 3  extend the number to be standard 6
+  convert String to hexadecimal to interger
+  multiply integers together to get seperate R G B   decimal values
+  */
+
+
+  // find the length of the colour.   Either 3 || 6
+  if (colourGuessValue.length == 3){
+    //if length is 3  extend the number to be standard 6
+
+    //set up variables used in this section
+    var stringSingleR, stringSingleG, stringSingleB;
+    var stringDoubleR, stringDoubleG, stringDoubleB;
+
+    //split the string into an array of single characters
+    colourGuessValue = colourGuessValue.split("");
+
+    //set stringSingle(R G B) into stringDouble(R G B)
+    stringDoubleR = colourGuessValue[0] + colourGuessValue[0];
+    stringDoubleG = colourGuessValue[1] + colourGuessValue[1];
+    stringDoubleB = colourGuessValue[2] + colourGuessValue[2];
+
+    colourGuessValue = stringDoubleR + stringDoubleG + stringDoubleB;
+    console.log("3 digit code turned into 6 digit: "+ colourGuessValue);
+  }
+  else{
+    //colourGuessValue is already standard 6 digit.
+    //Do nothing
+  }
+
+
+  //convert String to hexadecimal  -- to interger
+  colourGuessValue = colourGuessValue.toString(16);
+  console.log("colourGuessValue toString(16): "+colourGuessValue);
+
+
+  //multiply integers together to get seperate R G B   decimal values
+
+}
